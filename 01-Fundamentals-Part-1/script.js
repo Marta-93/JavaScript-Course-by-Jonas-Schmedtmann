e211bdc271cb35a7d995208c92b0600cef11d445
Marta Collu <marta.collu93@gmail.com>
2022-04-28 16:54:30 +0200

    add: assignment lecture values and variables

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,9 @@
+//Lecture: Values and variables
+
+let country = 'Italy';
+let continent = 'Europe';
+let population = '59.55 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
