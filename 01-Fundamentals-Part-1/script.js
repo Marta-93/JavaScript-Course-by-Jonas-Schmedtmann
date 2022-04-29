2cb0a0220c573b9b837c7ff885a56d8d27adb54c
Marta Collu <marta.collu93@gmail.com>
2022-04-29 11:21:59 +0200

    add: assignments lecture let, const and var

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,20 +1,41 @@
 //Lecture: Values and variables
+/*1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console*/
 
-let country = 'Italy';
-let continent = 'Europe';
+//let country = 'Italy';
+//let continent = 'Europe';
 let population = 59.55;
 
-console.log(country);
-console.log(continent);
+//console.log(country);
+//console.log(continent);
 console.log(population);
 
 //Lecture: Data Types
+/*1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console*/
 
-let isIsland = false;
+//let isIsland = false;
 let language;
 
-console.log(isIsland);
-console.log(population);
-console.log(country);
-console.log(language);
+//console.log(isIsland);
+//console.log(population);
+//console.log(country);
+//console.log(language);
 
+//Lecture: let, const and var
+/*1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens*/
+
+language = 'italian';
+const country = 'Italy';
+const continent = 'Europe';
+const isIsland = false;
+
+//isIsland = true;
