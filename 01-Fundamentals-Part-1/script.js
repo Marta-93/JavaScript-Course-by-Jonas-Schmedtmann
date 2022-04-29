ac51f7e8b1b8fca30ff6d8cca8051c432cb0c82d
Marta Collu <marta.collu93@gmail.com>
2022-04-29 14:35:34 +0200

    add: challenge 1

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -63,3 +63,4 @@ console.log(population > 33);
 const description1 = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
 
 console.log(description1);
+
